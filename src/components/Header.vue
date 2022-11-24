1dcd3098cc7eaa3c80ec5aceb5184a9bb8645274
jordypx <jordanodo89@gmail.com>
2022-11-24 15:37:44 -0700

    icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,25 @@
 <template>
-    <header>
+    <header class="fixed top-0 left-0 right-0 flex items-center justify-between">
 
-        <a href="#" class="logo"><font-awesome-icon icon="fa-solid fa-user-utensils" />pizzahub</a>
+       <div class="left-head flex items-center">
+        <a href="#" class="logo text-4xl font-extrabold text-black flex"><ion-icon name="pizza" class="text-orange-600 mb-2"></ion-icon><p class="">pizzahub</p></a>
 
-      <nav class="navbar">
-          <a href="#home">Home</a>
-          <a href="#dishes">dishes</a>
-          <a href="#about">About</a>
-          <a href="#menu">menu</a>
-          <a href="#reviews">reviews</a>
-          <a href="#orders">orders</a>    
+    <nav class="navbar">
+    <!-- <a href="#home">Home</a>
+    <a href="#dishes">dishes</a> -->
+    <a href="#menu">Menu</a>
+    <a href="#about">About</a>
+    <a href="#reviews">reviews</a>
+    <a href="#orders">orders</a>    
     </nav>
+       </div>
+
+    <div class="icons">
+        <ion-icon name="menu-outline" class="menu cursor-pointer"></ion-icon>
+        <ion-icon class="searcr cursor-pointer" name="search-outline"></ion-icon>
+        <a class=" " href="#"><ion-icon name="heart"></ion-icon></a>
+        <a class="" href="#"><ion-icon name="cart"></ion-icon></a>
+    </div>
     
     </header>
 </template>
